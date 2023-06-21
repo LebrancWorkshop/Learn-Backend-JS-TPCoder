aa0df856cdf9516d398604d0bf693faf8ba034c9
lebrancconvas <lebrancconvas@gmail.com>
2023-06-21 19:09:03 +0700

    ✅ test: update test case

--- unit-testing/test/character.test.js
+++ unit-testing/test/character.test.js
@@ -1,7 +1,7 @@
 import { checkPalindrome } from '../src/utils/character.js';
 
-test('Check Palindrome [1,0,1] return true', () => {
-  expect(checkPalindrome([1, 0, 1])).toBe(true);
+test('Check Palindrome [2, 0, 2] return true', () => {
+  expect(checkPalindrome([2, 0, 2])).toBe(true);
 });
 
 test('Check Palindrome [1,0,1,0] return false', () => {
